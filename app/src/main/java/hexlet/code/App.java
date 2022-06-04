55f0e1537f84d70f4080ec7cc3cf1c9804482bc6
Vladimir Tyan <Tyan.volodia@yandex.ru>
2022-06-04 10:46:17 +0300

    add build.gradle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,12 +6,12 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even \n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\nYour choice: ");
-        int n = in.nextInt();
-        if (n == 1) {
+        int numGame = in.nextInt();
+        if (numGame == 1) {
             Cli.greetings();
         }
-        else if ((n >= 2) && (n <= 6)) {
-            Engine.gameGames(n);
+        else if ((numGame >= 2) && (numGame <= 6)) {
+            Engine.gameGames(numGame);
         }
         else {
             System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even \n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\nYour choice: ");
