9031a0b4238e19413734a16e895c2d1c65f12aa0
Vova Tyan <Tyan.volodia@yandex.ru>
2022-06-18 09:24:15 +0300

    check project v5.1

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -6,7 +6,7 @@ import hexlet.code.Utils;
 public class GCD {
     private static final int MAX_RANDOM = 100;
     private static final String CONDITION = "Find the greatest common divisor of given numbers.";
-    private static String gcd (int number1, int number2) {
+    private static String gcd(int number1, int number2) {
         int j;
         for (j = number1; j >= 1; j--) {
             if (((number2 % j) == 0) && ((number1 % j) == 0)) {
@@ -17,7 +17,6 @@ public class GCD {
     }
     public static void startGame() {
         String[][] result = new String[Engine.COUNT_ROUNDS][2];
-
         for (int i = 0; i < Engine.COUNT_ROUNDS; i++) {
             var random1 = Utils.intRandom(1, MAX_RANDOM);
             var random2 = Utils.intRandom(1, MAX_RANDOM);
